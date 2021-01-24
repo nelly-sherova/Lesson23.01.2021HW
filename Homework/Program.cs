ddeff212c4bf134c8e7e97a459a29d1a41fa5ae2
unknown <nellysherova@mail.ru>
2021-01-24 11:17:16 +0300

    Added solution to first homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,7 +6,103 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+                Console.WriteLine("ДЗ 1 Задание 2");
+                Console.Write("A = "); int.TryParse(Console.ReadLine(), out int a);
+                Console.Write("B = "); int.TryParse(Console.ReadLine(), out int b);
+                int sum = 0;
+                for (int i  = a + 1; i < b; i++) sum += i;
+                Console.WriteLine($"Sum is {sum}");
+                Console.WriteLine($"Нечетные значения  расположенные между {a} и {b}");
+                for(int i  = a + 1; i < b; i++) if (i % 2 != 0) Console.Write($"{i} ");
+            }
+            {
+                Console.WriteLine("\nДЗ 1 Задание 3");
+                Console.WriteLine("Прямоугольник");
+                Console.Write("Введите длину прямоугольника: "); int.TryParse(Console.ReadLine(), out int a);
+                Console.Write("Введите ширину прямоугольника: "); int.TryParse(Console.ReadLine(), out int b);
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                for(int i=1; i<=a; i++)
+                {
+                    for(int j=1; j<=b; j++)
+                    {
+                       if ((i>1 && i<a) && (j>1 && j<b))Console.Write("  ");
+                       else Console.Write("* ");
+                    } 
+                    Console.WriteLine();
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            {
+                Console.WriteLine("Прямоугольный треугольник: ");
+                Console.Write("Введите длину катета: "); int.TryParse(Console.ReadLine(), out int a);
+                int b = a + 1;
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i=1; i<=a; i++)
+                {
+                    for (int j=1; j<=b; j++)
+                    {
+                        if (i>=1 && i==(j-1) && j<b) Console.Write("*");
+                        if (i>=1 && j==1) Console.Write("*");
+                        if (i==a) Console.Write("*");
+                        else Console.Write(" ");  
+                    }
+                    Console.WriteLine();
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            {
+                Console.WriteLine("Равносторонний треугольник ");
+                Console.Write("Введите сторону равностороннего треугольника: "); int.TryParse(Console.ReadLine(), out int a);
+                int t = a;
+                a = a*2+1;
+                int ai=1;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                for (int i=1; i<=t+1; i++)
+                {
+                    for (int j=1; j<=a; j++)
+                    {
+                        if (i==1 && j==a/2+1) Console.Write("*");
+                        if (i==ai && j==t+1-ai && i!=1) Console.Write("*");
+                        if (i==ai && j==t+1+ai-1 && i!=1) Console.Write("*");
+                        if (i==t && j%2>0 && j<a-1 && j!=1 && j!=a-2) Console.Write("*");
+                        else Console.Write(" ");      
+                    }
+                    Console.WriteLine();
+                    if(ai<t)ai++;   
+                }  
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            {
+                Console.WriteLine("Ромб");
+                Console.Write("Введите сторону ромба: "); int.TryParse(Console.ReadLine(), out int a);
+                a = a-1;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                for (int i=a; i>=-a; i--)
+                {
+                    for (int j=1; j<=Math.Abs(i); j++)Console.Write(" ");
+                    for(int k=a; k>=Math.Abs(i); k--)
+                    {
+                        Console.Write("*");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            {
+                Console.WriteLine("ДЗ1 Задание 4");
+                double s = 1000;
+                Console.Write("P = "); double.TryParse(Console.ReadLine(), out double p);    
+                int k=0;
+                do
+                {
+                    s =  s * (p / 100 + 1);
+                    k++;
+                }while (s <= 1100);
+                Console.WriteLine($"k = {k} ");
+                Console.WriteLine($"S = {s}");
+            }
         }
     }
 }
